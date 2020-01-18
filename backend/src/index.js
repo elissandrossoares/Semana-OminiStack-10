6802fd7337d55655eb08b00b374dd5e0bbb3e780
Elissandro Silva <elissandros667@gmail.com>
2020-01-17 21:42:12 -0300

    Adding project

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket }  = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://elissandro:elissandro@oministack-k2so4.mongodb.net/week10?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros: 
+// Query Params: req.query (Filtros, ordenaçãp, paginação, ..)
+// Route Params:  request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (Não-relacional)
+
+
